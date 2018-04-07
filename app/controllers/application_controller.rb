7b603c3bac62f008e44cce906bbc071d4cda0905
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-04-07 18:11:48 -0400

    added welcome controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def after_sign_out_path_for(resource_or_scope)
-    home_path
+  def after_sign_in_path_for(resource_or_scope)
+    request.env['omniauth.origin'] || welcome_path
   end
 
-  def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || welcome_path
+  def after_sign_out_path_for(resource_or_scope)
+    home_path
   end
 
 
