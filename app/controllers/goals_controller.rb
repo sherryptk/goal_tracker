eb4dbf5079360c8289aaed0306d0388c94adf05f
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-04-08 19:07:38 -0400

    added goals index page

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,15 +1,35 @@
 class GoalsController < ApplicationController
 
+  def index
+    @goals = Goal.all
+  end
   def new
   end
 
   def create
     @goal = Goal.create(goal_params)
-    redirect_to goal_path(@goal)
+    redirect_to @goal
   end
 
   def show
     @goal = Goal.find(params[:id])
+  end
+
+  def edit
+    @goal = Goal.find(params[:id])
+  end
+
+  def update
+    @goal = Goal.find(params[:id])
+
+    @goal.update(goal_params)
+
+   if @goal.save
+     redirect_to @goal
+   else
+     render :edit
+   end
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => 'sessions#create' }
+
   root 'welcome#index', as: 'root'
 
   get 'welcome/home', as: 'welcome'
@@ -9,7 +10,7 @@ Rails.application.routes.draw do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-  resources :goals, only: [:show, :index, :create, :new]
+  resources :goals
   resources :categories, only: [:show, :index, :create]
   # resources :users, only: [:show]
   # resources :carts
