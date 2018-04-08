9824cbc4a59c31a778521a6a702e2dc09302dc81
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-04-08 17:59:14 -0400

    added partial to goal form

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,7 +1,6 @@
 class GoalsController < ApplicationController
 
   def new
-    @goal = Goal.new
   end
 
   def create
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ActiveRecord::Base
   belongs_to :goal
-  accepts_nested_attributes_for :goals
 
 end
