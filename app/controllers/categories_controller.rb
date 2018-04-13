691b787354e4bfe9176ba3a146402c5716ee8988
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-04-13 17:50:03 -0400

    added goal by category scope method and added to category show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @goals = @category.goal_by_category
   end
 
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,12 @@ class Category < ActiveRecord::Base
   has_many :goals
   has_many :users, through: :goals
 
+  def goal_by_category
+    Goal.all.select do |goal|
+      if self.goal_id == goal.id
+        goal
+      end
+    end
+  end
+
 end
