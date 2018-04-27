03b788bbfbe8c2b22fa5ac8446f08f2b3e03542c
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-04-27 12:11:59 -0400

    fixed omniauth bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,9 @@ class User < ActiveRecord::Base
   has_many :categories, through: :goals
   has_many :tasks, through: :goals
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable,
-  :omniauthable, :omniauth_providers => [:facebook]
+  :omniauthable, :omniauth_providers => [:github]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
