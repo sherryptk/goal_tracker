a06e35cbc3667a0265a765cb9f598b8794ae8fe5
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-06-19 19:39:10 -0400

    fixed edit goal link issue

--- app/assets/javascripts/goals.js
+++ app/assets/javascripts/goals.js
@@ -10,4 +10,17 @@ $(function () {
   });
 });
 
+$(function () {
+  $(".js-next").on("click", function() {
+    var nextId = parseInt($(".js-next").attr("data-id")) + 1;
+    $.get("/goals/" + nextId + ".json", function(data) {
+      $(".goalTitle").text(data["data"]["attributes"]["title"]);
+      $(".goalDescription").text(data["data"]["attributes"]["description"]);
+      var editGoal = document.getElementsByClassName('js-edit')
+      $(editGoal).attr('href', '/goals/' + nextId + '/edit');
+      $(".js-next").attr("data-id", data["data"]["id"]);
+    });
+  });
+});
+
 })
