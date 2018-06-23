15f372adde556b32d4ba1d5744deb904960b4816
Sherry Elle <sherryelle@Sherrys-MacBook.local>
2018-06-23 09:47:13 -0400

    fixed bug in json fetch code

--- app/assets/javascripts/goals.js
+++ app/assets/javascripts/goals.js
@@ -13,11 +13,11 @@ $(function () {
   $(".js-next").on("click", function() {
     var nextId = parseInt($(".js-next").attr("data-id")) + 1;
     $.get("/goals/" + nextId + ".json", function(data) {
-      $(".goalTitle").text(data["data"]["attributes"]["title"]);
-      $(".goalDescription").text(data["data"]["attributes"]["description"]);
+      $(".goalTitle").text(data["goal"]["title"]);
+      $(".goalDescription").text(data["goal"]["description"]);
       var editGoal = document.getElementsByClassName('js-edit')
       $(editGoal).attr('href', '/goals/' + nextId + '/edit');
-      $(".js-next").attr("data-id", data["data"]["id"]);
+      $(".js-next").attr("data-id", data["goal"]["id"]);
     });
   });
 });
@@ -32,7 +32,6 @@ $(function () {
 
       posting.done(function(data) {
         var goal = new Goal(data["goal"]["title"], data["goal"]["description"], data["goal"]["categories"]["0"]["name"])
-
         $('form').hide();
         $('#pageHeading').hide();
         goal.postGoal();
